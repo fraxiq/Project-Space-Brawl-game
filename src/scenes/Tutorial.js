8bedc4fe88eca9ad8938c3c22b8c953b138dc89d
krasi1 <krissi.petrov@gmail.com>
2022-12-19 04:55:44 +0200

    adding animations

--- src/scenes/Tutorial.js
+++ src/scenes/Tutorial.js
@@ -4,8 +4,7 @@ import Scene from "./Scene";
 export default class Tutorial extends Scene {
   // TODO: Have to figure out how to call things from the preloader
   preload() {
-    const images = {
-    };
+    const images = {};
     const sounds = {};
     return super.preload({ images, sounds });
   }
@@ -13,7 +12,10 @@ export default class Tutorial extends Scene {
   //creating textures for all the slides so I can swap them later
   //needs to figure out preload so I can shorten this
   onCreated() {
-    let i = 0;
+    const ticker = Ticker.shared;
+    ticker.autoStart = false;
+    let counter = 0;
+    let pointerOver = false;
     const Slide1 = new PIXI.Texture.from(Assets._assets.Slide1);
     const Slide2 = new PIXI.Texture.from(Assets._assets.Slide2);
     const Slide3 = new PIXI.Texture.from(Assets._assets.Slide3);
@@ -24,19 +26,18 @@ export default class Tutorial extends Scene {
       PIXI.Sprite.from(Slide3),
     ];
 
-
     let currentSlide = new PIXI.Sprite.from(Assets._assets.Slide1);
-    currentSlide.anchor.set(0.5); 
+    currentSlide.anchor.set(0.5);
     console.log(this.background.width, this.background.height);
     currentSlide.height = 1060;
     currentSlide.y = 10;
-    
+
     this.addChild(currentSlide);
 
     const rightArrow = new PIXI.Sprite.from(Assets._assets.arrow);
     rightArrow.anchor.set(0.5);
     rightArrow.x = this.background.width / 2.5;
-    rightArrow.alpha = 0.6;
+    rightArrow.alpha = 0.5;
     rightArrow.interactive = true;
     rightArrow.buttonMode = true;
     this.addChild(rightArrow);
@@ -46,27 +47,70 @@ export default class Tutorial extends Scene {
     leftArrow.x = -this.background.width / 2.5;
     leftArrow.interactive = true;
     leftArrow.buttonMode = true;
-    leftArrow.alpha = 0.6;
+    leftArrow.alpha = 0.5;
+
+    rightArrow.on("pointerover", () => {
+      pointerOver = true;
+      if(pointerOver){
+      rightArrow.alpha = 1;
+      }
+    })
+
+    rightArrow.on("pointerout", ()=> {
+      pointerOver = false;
+      rightArrow.alpha = 0.5;
+    })
 
     rightArrow.on("pointerdown", () => {
-      if (i < Slides.length - 1) {
-        i++;
-        currentSlide.texture = Slides[i].texture;
+      if (counter < Slides.length - 1) {
+        counter++;
+        currentSlide.texture = Slides[counter].texture;
+        gsap.fromTo(
+          currentSlide.scale,
+          { x: 0.8, y: 0.8 },
+          { x: 1, y: 1, duration: 0.7, ease: "power2.out" }
+        );
       }
-      this.addChild(leftArrow);
 
+      this.addChild(leftArrow);
     });
 
-    leftArrow.on("pointerdown", () => {
-      if (i > 0) {
-        i--;
-        currentSlide.texture = Slides[i].texture;
+    leftArrow.on("pointerover", () => {
+      pointerOver = true;
+      if(pointerOver){
+      leftArrow.alpha = 1;
+      }
+    })
 
-        if (i == 0) {
-          currentSlide.texture = Slides[i].texture;
+    leftArrow.on("pointerout", ()=> {
+      pointerOver = false;
+      leftArrow.alpha = 0.5;
+    })
+
+
+    leftArrow.on("pointerdown", () => {
+      if (counter > 0) {
+        counter--;
+        currentSlide.texture = Slides[counter].texture;
+        gsap.fromTo(
+          currentSlide.scale,
+          { x: 0.8, y: 0.8 },
+          { x: 1, y: 1, duration: 0.7, ease: "power2.out" }
+        );
+
+        if (counter == 0) {
+          currentSlide.texture = Slides[counter].texture;
           this.removeChild(leftArrow);
         }
       }
     });
+
+    function incOp(img) {
+      img.alpha += 0.01;
+    }
+
+    function decOp(img) {
+      img.alpha -= 0.01;
+    }
   }
 }
